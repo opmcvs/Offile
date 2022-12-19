ed53c56949304194dc2611703c7d774a140ea5b7
opmcvs <111224956+opmcvs@users.noreply.github.com>
2022-12-19 15:58:53 +0800

    Update index.js

--- Questions Project/index.js
+++ Questions Project/index.js
@@ -1,8 +1,8 @@
 const btns= document.querySelectorAll('.question-btn')
 const questions = document.querySelectorAll('.question')
+
 btns.forEach(function(btn){
 btn.addEventListener('click', function(e){
-console.log (e.currentTarget.parentElement.parentElement)
 const question = e.currentTarget.
 parentElement.parentElement
 questions.forEach(function(item){
