556caaa52e920aeb2a616b99f5b024667dcf8156
opmcvs <111224956+opmcvs@users.noreply.github.com>
2022-12-19 14:52:24 +0800

    Update Blackjackindex.js

--- Card Game Black Jack/Blackjackindex.js
+++ Card Game Black Jack/Blackjackindex.js
@@ -1,69 +1,65 @@
 // 21 wins or as close as possible above your out.
 //Ace = 11 and king = 10
 //querySelector.for class #for Id;
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-const cards = [firstCard,secondCard];
-let sum = firstCard + secondCard;
-let hasBlackJack = false;
-let isAlive = true;
-let message = "";
-let newCard = getRandomCard();
-const startBtn = document.getElementById("start-btn");
-const messageEl = document.getElementById("message-el");
-const sumEl = document.getElementById("sum-el");
-const cardsEl = document.querySelector("#cards-el");
 
-const newcardBtn=document.getElementById("newcard-btn");
+let cards =[];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message ="";
+const messageEl =document.getElementById("message-el");
+const sumEl = document.getElementById("sum-el");
+const cardsEl =document.getElementById("cards-el");
+const startBtn = document.getElementById("start-btn")
+const newCardBtn  = document.getElementById("newcard-btn");
 
 
 function getRandomCard(){
-    let resultRandom = Math.floor(Math.random()*13)+1;
-    if(resultRandom>10){
-        return 10
-    }else if (resultRandom===1 ){
-        return 11
-    }else{
-        return resultRandom
-     }
-    }
-
-function startGame(){
-    renderGame();
+let randomNumber = Math.floor(Math.random()*13)+1;
+if (randomNumber >10){
+    return 10
+} else if (randomNumber === 1){
+    return 11
+} else{
+    return randomNumber
+}
 }
 
-    message = ("You're out of the game");
-    isAlive = false;
-startBtn.addEventListener("click",  renderGame);
-function renderGame() {
-cardsEl.textContent = `Cards: `;   
-// if you won't do this it will repeat the value in the 
 
-    for (let i=0; i<cards.length;i++){
-    cardsEl.textContent += cards[i] + " "
-}
+startBtn.addEventListener("click",function startGame(){
+ isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard,secondCard];
+    sum = firstCard + secondCard;
+    renderGame()
 
-sumEl.textContent= "Sum:"+ sum;
-
-if (sum<=20){
-    message = ("Do you want to draw a new card")
-}else if(sum === 21){
-    message = ("You won")
-    hasBlackJack = true  
-}else {
-    message = ("You're out of the game");
-    isAlive = false;
-}
-
-messageEl.textContent = message;
-}
-
-newcardBtn.addEventListener("click",()=>{
-    
-    sum += newCard;
-    cards.push(newCard);
-    renderGame();    
 })
 
+    function renderGame(){
+    cardsEl.textContent = "Cards:" ;
+    for (let i=0; i< cards.length; i++){
+        cardsEl.textContent += cards[i]+ " "
+     }
+     sumEl.textContent = "Sum:" + sum
+     if (sum <= 20){
+        message = "Do you want to draw a new card ?"
+     }else if (sum === 21){
+     message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
 
-    
+
+newCardBtn.addEventListener("click", function newCard(){
+
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+})
+
