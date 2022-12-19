f12733ca5992c4bbced0dd67e791d77c43d9e7c2
opmcvs <111224956+opmcvs@users.noreply.github.com>
2022-12-19 15:04:03 +0800

    Update index.js

--- Questions Project/index.js
+++ Questions Project/index.js
@@ -1,6 +1,3 @@
-//First solution is traversing the DOM(moving up and down the DOM tree).
-//Second option will be using the selectors.
-
 const btns= document.querySelectorAll('.question-btn')
 const questions = document.querySelectorAll('.question')
 btns.forEach(function(btn){
@@ -17,27 +14,3 @@ questions.forEach(function(item){
 question.classList.toggle('show-text')
 })
 })
-
-//Second option will be using the selectors.
-// targeting the Article directly
-/*const questions = document.querySelectorAll('.question')
-// You can add querySelector to an Element
-questions.forEach(function(question){
-console.log(question)
-
-const btn = question.querySelector('.question-btn')
-console.log(btn)
-btn.addEventListener('click',function(){
-
-    questions.forEach(function(item){
-    console.log(item)
-    if(item !==question){
-     item.classList.remove('show-text')
-    }
-    }) 
-
-
-
-    question.classList.toggle('show-text')
-})
-}) */
